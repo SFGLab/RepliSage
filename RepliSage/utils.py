ce5efd9cd499f894876bb5a806c3b0df8f11a956
Sebastian Korsak <sebykorsak@gmail.com>
2025-05-26 17:32:40 +0200

    improved heatmap plots

--- RepliSage/utils.py
+++ RepliSage/utils.py
@@ -525,14 +525,14 @@ def get_avg_heatmap(path, N1, N2):
 
     try:
         figure(figsize=(20, 20))
-        plt.imshow(mean_heat, cmap='Reds', vmax=0.2, aspect='auto')
+        plt.imshow(mean_heat, cmap='Reds_r', aspect='auto')
         plt.title('Mean Structure Heatmap')
         plt.savefig(path + f'/plots/structural_metrics/mean_heatmap_{N1}_{N2}.png', format='png', dpi=200)
         plt.savefig(path + f'/plots/structural_metrics/mean_heatmap_{N1}_{N2}.svg', format='svg', dpi=200)
         plt.close()
 
         figure(figsize=(20, 20))
-        plt.imshow(std_heat, cmap='Blues', aspect='auto')
+        plt.imshow(std_heat, cmap='gnuplot2_r', aspect='auto')
         plt.title('Std Structure Heatmap')
         plt.savefig(path + f'/plots/structural_metrics/std_heatmap_{N1}_{N2}.png', format='png', dpi=200)
         plt.savefig(path + f'/plots/structural_metrics/std_heatmap_{N1}_{N2}.svg', format='svg', dpi=200)
